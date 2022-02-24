d5a1d38295f67a556ec61738b484469e19ae1901
amritart073 <amritpaudel072@gmail.com>
2022-02-24 21:02:26 +0545

    mistakes were made

--- controllers/method_funcs.js
+++ controllers/method_funcs.js
@@ -25,6 +25,7 @@ const serveData = (req, res) => {
         } else {
             res.render('./blog-pages/blogs', { result: result })
         }
+        console.log(result[1].comment);
     })
 }
 const feedCom = (req, res) => {
@@ -41,5 +42,10 @@ const feedCom = (req, res) => {
     res.redirect('/')
 
 }
-module.exports = { createData, feedData, serveData, feedCom }
+
+const fetchCom = (req, res)=>{
+    console.log(req.body);
+    res.status(200)   
+}
+module.exports = { createData, feedData, serveData, feedCom, fetchCom }
 
--- routes/methods.js
+++ routes/methods.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const { createData, feedData, serveData, feedCom } = require('../controllers/method_funcs')
+const { createData, feedData, serveData, feedCom , fetchCom} = require('../controllers/method_funcs')
 
 
 router.get('/create', createData)
-
 
 router.post('/save', feedData)
 
 router.get('/', serveData)
 
 router.post('/feedCom', feedCom)
+
+router.post('/fetchCom', fetchCom)
 
 module.exports = router
