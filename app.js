a44ca24b31896a5e9cf372ffd607178db6159b21
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2022-03-21 10:47:52 +0545

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const methods = require('./routes/methods')
 const port = process.env.PORT || '5000';
+const cors = require('cors')
 
 //replace <password> with your database password
 dburl = 'mongodb+srv://amrit:root@cluster0.ec3ss.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
@@ -12,11 +13,11 @@ mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true }, (er
     app.listen(port,()=>{
       console.log(`Server started at port ${port}`)
     })
-  }
+  }var cors = require('cors')
 })
 
 const app = express()
-
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
 app.use('/', methods)
