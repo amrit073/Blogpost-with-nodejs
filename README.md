94821b3223bcbb8bef949766f7aff191a1b8d8b7
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2022-11-26 19:48:06 +0545

    docker

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 Simple blog Post creator using nodejs and express.
 It uses mongoose for updating and retriving data from MongoDB
 
+running container:
+
+```
+docker-compose up
+```
 
 deployed in Heroku: <a href="https://njpostblog.herokuapp.com/">njpostblog</a>
--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const methods = require('./routes/methods')
-const port = process.env.PORT || '5000';
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const methods = require("./routes/methods");
+const port = 3000;
+const cors = require("cors");
 
 //replace <password> with your database password
-dburl = 'mongodb+srv://amrit:root@cluster0.ec3ss.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-  if (err) { console.log(err); }
-  else {
-    console.log('we good')
-    app.listen(port,()=>{
-      console.log(`Server started at port ${port}`)
-    })
+dburl = "mongodb://mongo:27017/mydb";
+mongoose.connect(
+  dburl,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("we good");
+      app.listen(port, () => {
+        console.log(`Server started at port ${port}`);
+      });
+    }
   }
-})
+);
 
-const app = express()
-app.use(cors())
-app.use(express.urlencoded({ extended: false }))
-app.set('view engine', 'ejs')
-app.use('/', methods)
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.use("/", methods);
